8e286996fcd76b148cc31e5aee852ac06f296ff3
junbae.bae <bluesider@gmail.com>
2022-05-02 21:27:12 +0900

    Revise App's html

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  return <h1>Hello World!</h1>
+  return (
+      <div>
+        <h1>JunBae's Blog</h1>
+        <p>Welcome to the JB's funlog</p>
+      </div>
+  )
+
 }
 ReactDOM.render(<App/>, document.getElementById('root'))
