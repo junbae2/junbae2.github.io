ce9c4745599ff999a23be03a60838b4bb762b8b0
junbae.bae <bluesider@gmail.com>
2022-05-09 22:37:45 +0900

    Add a missed component js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+
+import Header from '../components/Header'
+import Navigation from '../components/Navigation'
 
 const Home = () => {
   return (
     <div>
-      <h1>Funlog</h1>
-      <p>This is the junbae's funlogs</p>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
+      <Header/>
+      <Navigation/>
+      <p>This is the Junbae's Funlog</p>
     </div>
   )
 }
