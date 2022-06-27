15690a5e383f8ede17211a886761e97347a03a42
junbae.bae <bluesider@gmail.com>
2022-06-27 22:37:31 +0900

    Apply style to Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,72 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Link } from 'react-router-dom'
+
+const Nav = styled.nav`
+  padding: 1em;
+  background: #f5f4f0;
+
+  @media (max-width: 700px) {
+    padding-top: 64px;
+  }
+
+  @media (min-width: 700px) {
+    position: fixed;
+    width: 200px;
+    height: calc(100% - 64px);
+  }
+`
+
+const NavList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  line-height: 2;
+
+  a {
+    text-decoration: none;
+    font-weight: bold;
+    font-size: 1.1em;
+    color: #333;
+  }
+
+  a:visited {
+    color: #333;
+  }
+
+  a:hover
+  a:focus {
+    color: #0077cc;
+  }
+`
 
 const Navigation = () => {
   return (
-    <div>
-      <ul>
+    <Nav>
+      <NavList>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">
+            <span aria-hidden="true" role="img">
+              🏠
+            </span>
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about">
+            <span aria-hidden="true" role="img">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                   fill="currentColor" className="bi bi-cart-fill"
+                   viewBox="0 0 16 16">
+                <path
+                  d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+              </svg>
+            </span>
+            About
+          </Link>
         </li>
-      </ul>
-    </div>
+      </NavList>
+    </Nav>
   )
 }
 
